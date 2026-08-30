665d3627c7075731330ce7060679cd930a3c68c7
Shawn <18322761430@163.com>
2026-08-30 13:05:53 +0800

    fix(android): handle system back in web routes

--- apps/android/app/src/main/java/app/lovable/bf11243b7233439588451aa876a26efd/MainActivity.java
+++ apps/android/app/src/main/java/app/lovable/bf11243b7233439588451aa876a26efd/MainActivity.java
@@ -1,5 +1,52 @@
 package app.lovable.bf11243b7233439588451aa876a26efd;
 
+import android.os.Bundle;
+
+import androidx.activity.OnBackPressedCallback;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    private static final String HANDLE_WEB_BACK =
+        "(function(){" +
+        "var dialog=document.querySelector('[role=\"dialog\"][data-state=\"open\"]');" +
+        "if(dialog){" +
+        "document.dispatchEvent(new KeyboardEvent('keydown',{key:'Escape',code:'Escape',bubbles:true,cancelable:true}));" +
+        "return true;" +
+        "}" +
+        "var index=window.history&&window.history.state&&window.history.state.idx;" +
+        "if(typeof index==='number'&&index>0){window.history.back();return true;}" +
+        "return false;" +
+        "})()";
+
+    private boolean handlingWebBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (handlingWebBack || getBridge() == null || getBridge().getWebView() == null) {
+                    runDefaultBack(this);
+                    return;
+                }
+
+                handlingWebBack = true;
+                getBridge().getWebView().evaluateJavascript(HANDLE_WEB_BACK, handled -> {
+                    handlingWebBack = false;
+                    if (!"true".equals(handled)) {
+                        runDefaultBack(this);
+                    }
+                });
+            }
+        });
+    }
+
+    private void runDefaultBack(OnBackPressedCallback callback) {
+        callback.setEnabled(false);
+        getOnBackPressedDispatcher().onBackPressed();
+        callback.setEnabled(true);
+    }
+}
